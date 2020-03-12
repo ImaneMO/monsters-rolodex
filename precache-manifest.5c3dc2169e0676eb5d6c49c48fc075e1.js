c39dc5afa8c672626be3fef2ad3c7369b144400b
unknown <imaneanbar7@gmail.com>
2020-03-12 15:35:08 +0100

    Updates

---
+++ precache-manifest.5c3dc2169e0676eb5d6c49c48fc075e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bc008b367315afa3e9aaffcc6f69e50",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "500485c965f721e3bfba",
+    "url": "/monsters-rolodex/static/css/main.ba155736.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "500485c965f721e3bfba",
+    "url": "/monsters-rolodex/static/js/main.b24e8570.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
